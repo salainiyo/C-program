f5ea1e6839807e4d944be8e51ffc56e67e3cace6
alain salvator <salainiyo2016@gmail.com>
2019-11-24 19:03:41 +0200

    another

---
+++ salary calculate/salary calculate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Employee1=new SalaryCalculator( 1, "james", "section");
+            Console.WriteLine("your net salary: " + SalaryCalculator.CalculateSalary(500000));
+            Console.WriteLine("your Hourly salary:"+SalaryCalculator.CalculateHourly(12, 34));
+            Console.WriteLine("your Hourly salary:"+ SalaryCalculator.CalculateDaily(12));
+            
+        }
+    }
+}
